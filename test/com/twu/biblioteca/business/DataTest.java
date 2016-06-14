f32fea299a54d4b302a6e8a2ea15a314449aaf5e
Isadora Barros <ibarros@thoughtworks.com>
2016-06-14 11:26:16 -0300

    Add test to findBookIndex.

--- test/com/twu/biblioteca/business/DataTest.java
+++ test/com/twu/biblioteca/business/DataTest.java
@@ -1,8 +1,7 @@
 package com.twu.biblioteca.business;
 
+import com.twu.biblioteca.exceptions.BookNotFoundException;
 import org.junit.Test;
-
-import java.util.Vector;
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,5 +9,12 @@ import static org.junit.Assert.assertEquals;
  * Created by ibarros on 6/13/16.
  */
 public class DataTest {
+    @Test
+    public void testFindBookIndex() throws BookNotFoundException {
+        Data data = new Data();
+        boolean isCheckedOut = false;
+        int index = data.findBookIndex("The Agile Samurai", isCheckedOut);
 
+        assertEquals(2, index);
+    }
 }
